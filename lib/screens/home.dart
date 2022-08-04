c56b4a174c753f6fcab3e1d50969408ffdd77323
Rafael Garcia <rafaeldk17@gmail.com>
2022-08-04 10:43:06 +0200

    fin seccion 2

---
+++ lib/screens/home.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+import '../models/band.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Band> bands = [
+    Band(id: '1', name: 'Metallica', votes: 5),
+    Band(id: '2', name: 'Queen', votes: 1),
+    Band(id: '3', name: 'Héroes del silencio', votes: 3),
+    Band(id: '4', name: 'Bon Jovi', votes: 2),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title:
+              const Text('BandNames', style: TextStyle(color: Colors.black87)),
+          backgroundColor: Colors.white,
+          elevation: 1,
+        ),
+        body: ListView.builder(
+            itemCount: bands.length,
+            itemBuilder: (context, i) => _bandTile(bands[i])),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          elevation: 1,
+          onPressed: addNewBand,
+        ));
+  }
+
+  Widget _bandTile(Band band) {
+    return Dismissible(
+      key: Key(band.id),
+      direction: DismissDirection.startToEnd,
+      onDismissed: (direction) {
+        print('direction: $direction');
+
+        // TODO: llamar el borrado en el server
+      },
+      background: Container(
+          padding: EdgeInsets.only(left: 8.8),
+          color: Colors.red,
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text('Delete Band', style: TextStyle(color: Colors.white)),
+          )),
+      child: ListTile(
+        leading: CircleAvatar(
+          child: Text(band.name.substring(0, 2)),
+          backgroundColor: Colors.blue[100],
+        ),
+        title: Text(band.name),
+        trailing: Text(
+          '${band.votes}',
+          style: TextStyle(fontSize: 20),
+        ),
+        onTap: () {
+          print(band.name);
+        },
+      ),
+    );
+  }
+
+  addNewBand() {
+    final textController = new TextEditingController();
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+            title: Text('New band name:'),
+            content: TextField(
+              controller: textController,
+            ),
+            actions: <Widget>[
+              MaterialButton(
+                child: Text('Add'),
+                elevation: 5,
+                textColor: Colors.blue,
+                onPressed: () => addBandTolist(textController.text),
+              )
+            ]);
+      },
+    );
+  }
+
+  void addBandTolist(String name) {
+    if (name.length > 1) {
+      this
+          .bands
+          .add(new Band(id: DateTime.now().toString(), name: name, votes: 0));
+      setState(() {});
+    }
+
+    Navigator.pop(context);
+  }
+}
